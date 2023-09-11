cf076334c8f2ac6a84bfd2023a5c0c1f127fbdbb
Pedro Henrique Furquim Gouveia <106687665+pedrohenriquefurquimgouveia@users.noreply.github.com>
2023-09-11 10:53:35 -0300

    Add files via upload
    aula 11/09

---
+++  main.js
@@ -0,0 +1,15 @@
+fuction tocaSomPom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto 
+while(contador< listaDeTeclas.length){
+    const tecla= listaDeTeclas[contador];
+    const.instrumento = tecla.classList[1];
+    
+}
+
