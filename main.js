883bcd37624aee21eab386d218fa8d139400a8b9
Pedro Henrique Furquim Gouveia <106687665+pedrohenriquefurquimgouveia@users.noreply.github.com>
2023-09-18 10:55:57 -0300

    Aula 18/09

---
+++ main.js
@@ -0,0 +1,24 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+    
+    tecla.onkeydow = fuinction() {
+        tecla.classList.add('ativa');
+    }
+
+    
+    
+}
